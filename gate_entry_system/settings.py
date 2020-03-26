a58dd4832c5ff962f8a066f19ee89864225c0c0f
Yernar Kambar <ernar.k.20@gmail.com>
2020-03-26 19:50:04 +0600

    made migrations

--- gate_entry_system/settings.py
+++ gate_entry_system/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gate_entry_app.apps.GateEntryAppConfig',
 ]
 
 MIDDLEWARE = [
