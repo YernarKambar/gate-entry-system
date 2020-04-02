3f002dc7aa87fe6a3fc1fed3c7dac5b3b260929b
Yernar Kambar <ernar.k.20@gmail.com>
2020-04-02 21:35:52 +0600

    added static_root in setting.py

--- gate_entry_system/settings.py
+++ gate_entry_system/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'gate_entry_app/static')
 LOGIN_REDIRECT_URL = '/'
